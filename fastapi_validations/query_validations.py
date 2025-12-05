2e2fb771f12a68bb2fb4dc7d004807450f4b6752
Elvin Cooper <ing.elvin01cooper@gmail.com>
2025-12-05 17:29:33 -0400

    query_validator: validaciones con string y nuemeros

--- fastapi_validations/query_validations.py
+++ fastapi_validations/query_validations.py
@@ -1,11 +1,37 @@
 from fastapi import FastAPI, Query
 from typing import Annotated
 
+# Validaciones con Strings:
+# max_length
+# min_length
+# pattern
+
+# Validaciones con Numeros (enteros y float)
+# gt greather than
+# ge greather than or equal
+# lt less than
+# le less than or equal
+
 app = FastAPI()
 
+
+
+#@app.get('/items')
+# async def read_items(q: Annotated[list[str] | None, Query()] = None):
+# 	results: dict = {"message": "Acesso a get(read_items)"}
+# 	if q:
+# 		results.update({"q": q})
+#     return results		
+
+# async def read_items(q: Annotated[int | None, Query(min_length=8)] = None):
+# 	results: dict = {"message": "Acesso a get(read_items)"}
+# 	if q:
+# 		results.update({"q": q})
+#     return results		
+
 @app.get('/items')
-async def read_items(q: Annotated[list[str] | None, Query()] = None):
+async def read_items(q: Annotated[list[str] | None, Query(gt=3)] = None):
 	results: dict = {"message": "Acesso a get(read_items)"}
 	if q:
 		results.update({"q": q})
-     return results		
+	return results    		
